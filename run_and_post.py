f2b3c04ad61eef00dbec8e1bc7d30117480a7ec5
David Bieber <dbieber@princeton.edu>
2014-09-14 23:12:09 -0700

    run and post

---
+++ run_and_post.py
@@ -0,0 +1,12 @@
+import scratch
+
+def main():
+    scratch.init()
+    scratch.newGame()
+    scratch.printAll()
+
+    scratch.simulateGame()
+    scratch.postGameToTumblr()
+
+if __name__ == "__main__":
+    main()
